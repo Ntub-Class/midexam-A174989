83e29a05019510e679344e3e5766bcc6c6aed938
A174989 <55239570+A174989@users.noreply.github.com>
2019-11-07 20:21:40 +0800

    Add files via upload

--- src/q2.ts
+++ src/q2.ts
@@ -1,3 +1,14 @@
 // 題目說明: 請使用套件readline-sync跟node-qrcode，輸入文字來產生QRCODE
 // 輸入說明: 請打上任意文字，並產生QRCODE
 // 輸出說明: 請輸出檔名為QRCODE.png圖片
+
+
+import * as realineSync from 'readline-sync'; 
+import * as QRCode from 'qrcode';//create QRcode
+ 
+ 
+let fileName = realineSync.question('QRCODE');
+let show = realineSync.question('今天期中考');
+ 
+QRCode.toFile('./' + fileName + '.png', show)
+console.log('Create QRcode successfully!')
--- src/q3.ts
+++ src/q3.ts
@@ -4,3 +4,39 @@
 // Bird class說明: 請複寫父類別的eat方法，呼叫Bird的eat方法時必須先印出 I override it，
 //  然後再呼叫父類別的eat 輸出 I'm {name} , I can eat~~
 // 最後請建立一個Bird實體，並呼叫eat跟fly方法
+class Animal {
+    //建構子
+    constructor(
+        public name: string,
+        public age: number) {
+    }
+
+    eat() {
+        console.log('im name',this.name, 'I can eat~~');
+    }
+}
+
+class Bird extends Animal {
+    //建構子
+    constructor(name: string, age: number) {
+        super(name, age)
+    }
+
+    fly() {
+        console.log('I can fly');
+    }
+    eat() {
+        super.eat();
+        console.log(' I override it');
+    }
+}
+
+let bird1 = new Bird('金絲雀', 20);
+let bird2 = new Bird('燕子', 21);
+
+//print bird1
+bird1.eat()
+bird1.fly()
+//print bird2
+bird2.eat()
+bird2.fly()
